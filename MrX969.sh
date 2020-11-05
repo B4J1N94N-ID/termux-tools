b84eeba155d8b7234fe69a709a6401410c22e8fd
B4J1N94N-ID <73929515+B4J1N94N-ID@users.noreply.github.com>
2020-11-05 14:24:46 +0800

    Update MrX969.sh

--- MrX969.sh
+++ MrX969.sh
@@ -8,13 +8,13 @@ white='\033[37;1m'
 yellow='\033[33;1m'
 sleep 1
 echo
-figlet "Welcome To Tools-Termux"
+toilet -f big -F gay Welcome
 sleep 1
-echo $yellow" ================================================================================= "
+echo $yellow" ========================================================== "
 echo $green" Author : Mr.X969 "
 echo $green" Team : Popeye "
 echo $green" Whatsapp : 085397907992 "
-echo $yellow" ================================================================================= "
+echo $yellow" ========================================================== "
 echo
 echo
 sleep 1
@@ -50,8 +50,17 @@ read -p "[Ketikan Nomor Script Yang Di Inginkan]>>" bro
 if [ $bro = 1 ] || [ $bro = 1 ]
 then
 clear
-figlet "Tunggu Sebentar..."
-echo $green"Script Weeman By evait-security"
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[32;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay Wait
+sleep 0.5
 git clone https://github.com/evait-security/weeman
 cd weeman
 python2 weeman.py
@@ -68,8 +77,17 @@ fi
 if [ $bro = 3 ] || [ $bro = 3 ]
 then
 clear
-figlet "Tunggu Sebentar..."
-echo $green"Termux Style By B4N954N2-ID"
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[32;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay Wait
+sleep 0.5
 git clone https://github.com/B4N954N2-ID/termux-style
 cd termux-style
 python2 run.py
@@ -77,8 +95,17 @@ fi
 if [ $bro = 4 ] || [ $bro = 4 ]
 then
 clear
-figlet "Tunggu Sebentar..."
-echo $green"Tools DarkFly By Ranginang67"
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[32;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay Wait
+sleep 0.5
 git clone https://github.com/Ranginang67/DarkFly-Tool
 cd DarkFly-Tool
 chmod +x *
@@ -88,8 +115,17 @@ fi
 if [ $bro = 5 ] || [ $bro = 5 ]
 then
 clear
-figlet "Tunggu Sebentar..."
-echo $green"Script CCTV By GUNAWAN18ID"
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[32;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay Wait
+sleep 0.5
 git clone https://github.com/GUNAWAN18ID/cctv
 pip2 install requests
 cd cctv
@@ -110,8 +146,17 @@ fi
 if [ $bro = 7 ] || [ $bro = 7 ]
 then
 clear
-figlet "Tunggu Sebentar..."
-echo $green"Script SQLMAP By sqlmapproject"
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[32;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay Wait
+sleep 0.5
 git clone https://github.com/sqlmapproject/sqlmap
 cd sqlmap
 python2 sqlmap.py
@@ -119,8 +164,17 @@ fi
 if [ $bro = 8 ] || [ $bro = 8 ]
 then
 clear
-figlet "Tunggu Sebentar..."
-echo $green"Creater Virus By viruz059"
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[32;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay Wait
+sleep 0.5
 git clone https://github.com/viruz09/CreaterVirus
 cd CreaterVirus
 python2 creater.py
@@ -128,8 +182,17 @@ fi
 if [ $bro = 9 ] || [ $bro = 9 ]
 then
 clear
-figlet "Tunggu Sebentar..."
-echo $green"Tools B4J1N94N By DarknessCyberTeam"
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[32;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay Wait
+sleep 0.5
 git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5
 cd B4J1N64Nv5
 sh B4J1N64N.sh
@@ -137,8 +200,17 @@ fi
 if [ $bro = 10 ] || [ $bro = 10 ]
 then
 clear
-figlet "Tunggu Sebentar..."
-echo $green"Tools Hack Facebook By FR13ND8"
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[32;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay Wait
+sleep 0.5
 git clone https://github.com/FR13ND8/BRUTEFORCEnew
 cd BRUTEFORCEnew
 sh new.sh
@@ -147,7 +219,16 @@ fi
 if [ $bro = 0 ] || [ $bro = 0 ]
 then
 clear
-figlet "Selamat Tinggal :)"
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[32;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay GoodBye
 sleep 1
 exit
 fi
