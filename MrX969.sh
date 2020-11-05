43f7ad951fd8be72b344f0d1e137475551ef2f7e
B4J1N94N-ID <73929515+B4J1N94N-ID@users.noreply.github.com>
2020-11-05 09:26:07 +0800

    Create MrX969.sh

---
+++ MrX969.sh
@@ -0,0 +1,153 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[32;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+figlet "Welcome To My Tools"
+sleep 1
+echo $yellow" ========================================================== "
+echo $green" Author : Mr.X969 "
+echo $green" Team : Popeye "
+echo $green" Whatsapp : 085397907992 "
+echo $yellow" ========================================================== "
+echo
+echo
+sleep 1
+echo $yellow"[+] Menu Tools"
+echo
+sleep 1
+echo $cyan"[1] Install Script Weeman"
+sleep 0.5
+echo $blue"[2] DDOS ( ERROR )"
+sleep 0.5
+echo $green"[3] Tampilan termux (jika di suruh masukkan password, paswordnya adalah : QWERTY"
+sleep 0.5
+echo $yellow"[4] 540 Tools DarkFly"
+sleep 0.5
+echo $white"[5] Hack CCTV menggunakan Termux dan Puffin Browser"
+sleep 0.5
+echo $cyan"[6] Script RedHawk"
+sleep 0.5
+echo $yellow"[7] Script SQLMAP untuk deface dll."
+sleep 0.5
+echo $blue"[8] Virus Creater (Script Pembuat Virus)"
+sleep 0.5
+echo $red"[9] Script B4J1N94N"
+sleep 0.5
+echo $cyan"[10] Hack Facebook Tertarget"
+sleep 0.5
+echo $green"[0] Exit"
+sleep 0.5
+echo
+echo $green
+read -p "[Ketikan Nomor Script Yang Di Inginkan]>>" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "Tunggu Sebentar..."
+echo $green"Script Weeman By evait-security"
+git clone https://github.com/evait-security/weeman
+cd weeman
+python2 weeman.py
+fi
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+figlet "Di Bilangin Error"
+sleep 0.5
+echo $green"Plis Lah Ya Kalau Udh Di Bilang Error Ya Error Jangan Ngeyel_-"
+sleep 6
+sh MrX969.sh
+fi
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+figlet "Tunggu Sebentar..."
+echo $green"Termux Style By B4N954N2-ID"
+git clone https://github.com/B4N954N2-ID/termux-style
+cd termux-style
+python2 run.py
+fi
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+figlet "Tunggu Sebentar..."
+echo $green"Tools DarkFly By Ranginang67"
+git clone https://github.com/Ranginang67/DarkFly-Tool
+cd DarkFly-Tool
+chmod +x *
+python2 install.py
+DarkFly
+fi
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+figlet "Tunggu Sebentar..."
+echo $green"Script CCTV By GUNAWAN18ID"
+git clone https://github.com/GUNAWAN18ID/cctv
+pip2 install requests
+cd cctv
+chmod +x *
+python2 scanner.py
+fi
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+figlet "Tunggu Sebentar..."
+echo $green"Script RedHawk By Tuhinshubhra"
+pkg install php
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+chmod +x rhawk.php
+cd RED_HAWK
+php rhawk.php
+fi
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+figlet "Tunggu Sebentar..."
+echo $green"Script SQLMAP By sqlmapproject"
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+python2 sqlmap.py
+fi
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+figlet "Tunggu Sebentar..."
+echo $green"Creater Virus By viruz059"
+git clone https://github.com/viruz09/CreaterVirus
+cd CreaterVirus
+python2 creater.py
+fi
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+figlet "Tunggu Sebentar..."
+echo $green"Tools B4J1N94N By DarknessCyberTeam"
+git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5
+cd B4J1N64Nv5
+sh B4J1N64N.sh
+fi
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+figlet "Tunggu Sebentar..."
+echo $green"Tools Hack Facebook By FR13ND8"
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+cd BRUTEFORCEnew
+sh new.sh
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+figlet "Selamat Tinggal :)"
+sleep 1
+exit
+fi
